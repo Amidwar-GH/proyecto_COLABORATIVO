e77e0aad39b53e6a1af76124b2457ff83db2cb94
Amidwar <amidwar05@gmail.com>
2025-06-05 22:46:43 -0500

    Agregando peticion de nombre al usuario

--- EJERCIODESECUENCIALESNOTAS.cpp
+++ EJERCIODESECUENCIALESNOTAS.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 int main(){
 	float EC,ED,EP,CF; //EV EVIDENCIA DE CONOCIMIENTO ED EVIDENCIA DE DESEMPEÑO EP EVIDENCIA DE PRODUCTO
+	char nombre[50];
+	
+	cout<<"Digite su nombre: "; cin>>nombre;
 	cout<<"Digite la calificacion de conocimiento: "; cin>>EC;
-	cout<<"Digite la calificacion de desempeño: "; cin>>ED;
+	cout<<"Digite la calificacion de desempenio: "; cin>>ED;
 	cout<<"Digite la calificacion de producto: "; cin>>EP;
 	CF= (EC*0.4)+(ED*0.35)+(EP*0.25);
-	cout<<"Su calificacion final es: "<<CF<<endl;
+	cout<<nombre<<"Su calificacion final es: "<<CF<<endl;
 	
 	
 	return 0;
