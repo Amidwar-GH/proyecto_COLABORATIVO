14ba0d19e15bc40fbe3967aa5e32eac469be5187
JackQuenta <jackalvaroqm@gmail.com>
2025-06-09 10:22:48 -0500

    Se modifico el Tercer_EjercicioSecuenciales del repositorio de proyecto_COLABORATIVO: Mejora la interfaz del usuario permitiendole ver el enunciado y comentario explicativo, se coloca validadores pra que los datos no sean negativos

--- Tercer_EjercicioSecuenciales.cpp
+++ Tercer_EjercicioSecuenciales.cpp
@@ -1,13 +1,18 @@
+//Ejercicio 3: Determinar el área de un pentágono regular.
 #include <iostream>
-
 using namespace std;
 int main(){
-	int perimetro, apotema, area;
-	cout<<"ingrese el valor del perimetro del pentagono"<<endl;
-	cin>>perimetro;
-	cout<<"ingrese el valor de la apotema del pentagono"<<endl;
-	cin>>apotema;
-	area=perimetro*apotema/2;
-	cout<<"el area del pentagono es: "<<area<<endl;
+	float lado, apotema, area; //se puso float para incluir decimales.
+	cout<<"Para hallar el area de un pentagono regular se necesitan los siguientes datos: "<<endl;
+	cout<<"medida del lado del pentagono regular: "<<endl;
+	cin>>lado;
+	if (lado >= 0){
+		cout<<"apotema del pentagono regular: "<<endl;
+		cin>>apotema;
+		if (apotema >= 0){
+			area = (lado * apotema)*5/2;
+			cout<<"El area del pentagono regular es: "<<area<<endl;
+		}
+	}
 	return 0;
 }
